263ac1fed8d7173b4087324b5cd477113c3d52e8
Lucas Tabosa Martins de Lima <lucasdapoint@hotmail.com>
2025-06-20 17:17:47 -0400

    0.0.7 - Ajuste no Index

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -47,7 +47,7 @@
 
     <flux:navlist variant="outline">
         <flux:navlist.item icon="cog-6-tooth" href="/configuracoes">Configurações</flux:navlist.item>
-        <flux:navlist.item icon="information-circle" href="#">Ajuda / <i>0.0.6</i></flux:navlist.item>
+        <flux:navlist.item icon="information-circle" href="#">Ajuda / <i>0.0.7</i></flux:navlist.item>
     </flux:navlist>
 
     <flux:dropdown position="top" align="start" class="max-lg:hidden">
--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,7 +1,19 @@
 <div>
     <flux:heading size="xl" level="1">Sentinela</flux:heading>
 
-    <flux:subheading size="lg" class="mb-6">Here's what's new today</flux:subheading>
+    <flux:subheading size="lg" class="mb-6">Monitore, audite e organize os processos da sua empresa com eficiência.</flux:subheading>
 
-    <flux:separator variant="subtle" />
+    <flux:separator variant="subtle"/>
+
+    <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-5">
+
+        </div>
+        <div class="col-span-7 flex mt-8">
+            <img src="{{ asset('imagens/devprod_white.svg') }}" class="block dark:hidden">
+
+            <!-- Tema escuro -->
+            <img src="{{ asset('imagens/devprod_black.svg') }}" class="hidden dark:block">
+        </div>
+    </div>
 </div>
